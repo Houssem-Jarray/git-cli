9579c7a06c8b5ffd7fe0432d05f58e5495c42687
houssem-jarray <houssemjarray315@gmail.com>
2024-04-15 10:15:46 +0100

    first commit

---
+++ cmd/base.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	b64 "encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "get",
+	Short: "get repo details",
+	Long:  `Get Repo information using the Cobra Command`,
+	Run: func(cmd *cobra.Command, args []string) {
+		username, _ := rootCmd.Flags().GetString("username")
+		password, _ := rootCmd.Flags().GetString("password")
+		auth := fmt.Sprintf("%s:%s", username, password)
+		authEncode := b64.StdEncoding.EncodeToString([]byte(auth))
+
+		url := "https://api.github.com/user/repos"
+		method := "GET"
+
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, nil)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		req.Header.Add("Authorization", fmt.Sprintf("Basic %s", authEncode))
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer res.Body.Close()
+
+		var response []interface{}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		err = json.Unmarshal(body, &response)
+
+		fmt.Println("Response written to response.json successfully")
+		for _, repoDetails := range response {
+			repo := repoDetails.(map[string]interface{})
+			fmt.Println(" name : ", repo["name"], " private :", repo["private"], "clone_url :", repo["clone_url"])
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+	rootCmd.PersistentFlags().StringP("username", "u", "", "the username of git")
+	rootCmd.PersistentFlags().StringP("password", "p", "", "the access token of the git")
+}
